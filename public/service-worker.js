71bb8dc70428c2f93d7b82d309b1ac543892fa2f
talhaansari75 <talhaansari75@gmail.com>
2026-09-13 01:32:58 +0500

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'lexora-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.map((name) => { if (name !== CACHE_NAME) return caches.delete(name); }))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
